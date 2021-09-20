9d85ec5c9db525f58f0884e7bdeeb5675cb1f2b7
rohitdas13595 <rohitdas10595@gmail.com>
2021-09-20 21:06:09 +0530

    sixth commit

--- src/index.js
+++ src/index.js
@@ -7,18 +7,6 @@ const port = process.env.PORT
 
 
 
-// app.use((req,res,next)=>{
-//     if(req.method=='GET'){
-//         res.send('GET requests are disabled')
-//     }else{
-//         next();
-//     }
-    
-// })
-
-// app.use((req,res,next)=>{
-//     res.status(503).send('Site is under maintainance. Check back soon!')
-// })
 
 app.use(express.json())
 app.use(userRouter);
@@ -28,16 +16,6 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-// const Task =require ('./models/task')
-// const User=require('./models/user')
+const Task =require ('./models/task')
+const User=require('./models/user')
 
-// const main = async()=>{
-    
-    // const task= await Task.findById('614431bc7272b4647af30a61')
-    // await task.populate('owner')
-    // console.log(task.owner);
-    // const user= await User.findById('6144309e2535d37d79fbcd88')
-    // await user.populate('tasks')
-    // console.log(user.tasks);
-// }
-// main()
