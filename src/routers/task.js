93f77d3a15c40b394c71f6c62d66ba59f401f5a9
rohitdas13595 <rohitdas10595@gmail.com>
2021-09-20 21:24:03 +0530

    7 commit

--- src/routers/task.js
+++ src/routers/task.js
@@ -23,26 +23,26 @@ router.post('/tasks', auth, async (req, res) => {
 // GET /tasks?sortBy=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
-    const sort = {}
+    //const sort = {}
 
     if (req.query.completed) {
         match.completed = req.query.completed === 'true'
     }
 
-    if (req.query.sortBy) {
-        const parts = req.query.sortBy.split(':')
-        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
-    }
+    // if (req.query.sortBy) {
+    //     const parts = req.query.sortBy.split(':')
+    //     sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    // }
 
     try {
         await req.user.populate({
             path: 'tasks',
             match,
-            options: {
-                limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip),
-                sort
-            }
+            // options: {
+            //     limit: parseInt(req.query.limit),
+            //     skip: parseInt(req.query.skip),
+            //     //sort
+            // }
         })
         res.send(req.user.tasks)
     } catch (e) {
