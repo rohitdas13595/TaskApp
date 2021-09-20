e619ba0edb799bfa18620349f3ac3bede305340e
rohitdas13595 <rohitdas10595@gmail.com>
2021-09-20 21:14:52 +0530

    7 commit

--- src/routers/task.js
+++ src/routers/task.js
@@ -43,10 +43,10 @@ router.get('/tasks', auth, async (req, res) => {
                 skip: parseInt(req.query.skip),
                 sort
             }
-        })
+        }).execPopulate();
         res.send(req.user.tasks)
     } catch (e) {
-        res.status(500).send()
+        res.status(500).send(e)
     }
 })
 
